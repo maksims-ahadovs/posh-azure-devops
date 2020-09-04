dd76af146660d581932ba71fdf2c69d5c855c007
Ahadovs, Maksims <1160025+maksims-ahadovs@users.noreply.github.com>
2020-09-04 17:18:30 +0200

    Removed script file extensions

--- Invoke-YamlPipeline.ps1
+++ Invoke-YamlPipeline.ps1
@@ -19,13 +19,13 @@ function Invoke-YamlPipeline (
     $Context = $AzureDevOpsContext
 )
 {
-    $initialRunResult = Start-YamlPipeline.ps1 -PipelineId $PipelineId -YamlTemplateParameters $YamlTemplateParameters -FullBranchReferenceName $FullBranchReferenceName -Context $Context
+    $initialRunResult = Start-YamlPipeline -PipelineId $PipelineId -YamlTemplateParameters $YamlTemplateParameters -FullBranchReferenceName $FullBranchReferenceName -Context $Context
 
     Write-Verbose -Message "Pipeline run id is '$($initialRunResult.Id)'."
 
     for ($timeSlept = 0; $timeSlept -lt $TimeoutSeconds; $timeSlept += $PollingIntervalSeconds)
     {
-        $runResult = Get-YamlPipelineRun.ps1 -PipelineId $PipelineId -RunId $initialRunResult.Id -Context $Context
+        $runResult = Get-YamlPipelineRun -PipelineId $PipelineId -RunId $initialRunResult.Id -Context $Context
 
         Write-Verbose -Message "Pipeline run state is '$($runResult.State)'."
 
