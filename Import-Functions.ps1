bc2b2c211f23491b5c63769fdb70a21124cb0d7f
Ahadovs, Maksims <1160025+maksims-ahadovs@users.noreply.github.com>
2020-09-08 14:09:26 +0200

    Moved context related functions into separate directory

--- Import-Functions.ps1
+++ Import-Functions.ps1
@@ -1,7 +1,8 @@
+. $PSScriptRoot/Context/Get-Context.ps1
+. $PSScriptRoot/Context/Set-Context.ps1
+
 . $PSScriptRoot/Build-YamlPipelineTemplate.ps1
-. $PSScriptRoot/Get-Context.ps1
 . $PSScriptRoot/Get-YamlPipeline.ps1
 . $PSScriptRoot/Get-YamlPipelineRun.ps1
 . $PSScriptRoot/Invoke-YamlPipeline.ps1
-. $PSScriptRoot/Set-Context.ps1
 . $PSScriptRoot/Start-YamlPipeline.ps1
