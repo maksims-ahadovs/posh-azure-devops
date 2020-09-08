26b1a40249111f4f56ad546904cbf3e3882b05b6
Ahadovs, Maksims <1160025+maksims-ahadovs@users.noreply.github.com>
2020-09-08 14:21:15 +0200

    Moved pipeline related function into separate directory

--- Import-Functions.ps1
+++ Import-Functions.ps1
@@ -1,10 +1,10 @@
 . $PSScriptRoot/Context/Get-Context.ps1
 . $PSScriptRoot/Context/Set-Context.ps1
 
+. $PSScriptRoot/YamlPipeline/Get-YamlPipeline.ps1
+
 . $PSScriptRoot/YamlPipelineRun/Get-YamlPipelineRun.ps1
 . $PSScriptRoot/YamlPipelineRun/Invoke-YamlPipelineRun.ps1
 . $PSScriptRoot/YamlPipelineRun/Start-YamlPipelineRun.ps1
 
 . $PSScriptRoot/YamlPipelineTemplate/Build-YamlPipelineTemplate.ps1
-
-. $PSScriptRoot/Get-YamlPipeline.ps1
